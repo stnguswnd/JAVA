9c968815eb73c26cdf102c41f3daa140ff6950d4
stnguswnd <stnguswnd@gmail.com>
2025-11-03 15:17:10 +0900

    커밋

---
+++ src/a/basic/Operator.java
@@ -0,0 +1,23 @@
+package a.basic;
+
+public class Operator {
+    public static void main(String[] args) {
+        int intA = 14;
+        int intB = 6;
+        System.out.println(intA & intB);
+
+        int inA =14;
+        int inB = 6;
+        System.out.println(inA & intB);
+
+        int intC = 123456;
+        System.out.println(intC << 1);
+        System.out.println(intC >> 1);
+
+        System.out.println(intC >> 1);
+        System.out.println(intC >>> 1);
+
+
+
+    }
+}
---
+++ src/a/basic/Variable.java
@@ -0,0 +1,21 @@
+package a.basic;
+
+public class Variable {
+    public static void main(String[] args) {
+
+        // 선언
+        // 타입 변수명 ;
+        int age;
+
+        // 할당
+        //변수명 = 값;
+        age = 20;
+
+        System.out.printf("나이 %d", age);
+
+        //선언 + 할당 같이
+        String name = "KIM";
+        System.out.printf("이름 %s", name);
+
+    }
+}
